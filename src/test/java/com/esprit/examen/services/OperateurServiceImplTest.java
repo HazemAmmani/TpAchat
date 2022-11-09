8fc9d0f798d66f5af605a5ca8a033d53b1f89282
abdelhamidatallah <abdelhamid.atallah@esprit.tn>
2022-11-09 17:29:13 +0100

    Final test

--- src/test/java/com/esprit/examen/services/OperateurServiceImplTest.java
+++ src/test/java/com/esprit/examen/services/OperateurServiceImplTest.java
@@ -5,6 +5,10 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Order;
 import org.junit.Test;
@@ -22,7 +26,6 @@ import com.esprit.examen.repositories.OperateurRepository;
 
 @SpringBootTest(classes= TpAchatProjectApplication.class)
 @RunWith(MockitoJUnitRunner.class)
-
 public class OperateurServiceImplTest {
 	
 	
@@ -36,24 +39,6 @@ public class OperateurServiceImplTest {
 		OperateurServiceImpl operateurService;
 	
 	
-	 
-	
-//	 @BeforeEach
-//	    void setMockOutput() {
-//		 when(operateurRepository.save(operateur1)).thenReturn(operateur1);
-//	    }
-	
-	 
-	
-//	@Test
-//	public void retrieveAllOperateursTest() {
-//	
-//	
-//    	when(operateurRepository.findAll()).thenReturn(Stream
-//    			.of(new Operateur(1L,"test1","tes1","546125",null),new Operateur(2L,"test2","test2","546125",null))
-//   			.collect(Collectors.toList())); 
-//   	assertEquals(2, operateurService.retrieveAllOperateurs().size());
-//   	}
 	  @Test
 	  @Order(1)
 		public void addOperateurTest() {
@@ -66,34 +51,34 @@ public class OperateurServiceImplTest {
 		System.out.println("add operators works !");
 }
 
-// @Test 
-// @Order(2)
-//	   public void retrieveAllOperateursTest() {
-//	    	when(operateurRepository.findAll()).thenReturn(Stream
-//	    			.of(operateur1,operateur2)
-//	    			.collect(Collectors.toList()));
-//	    	
-//	    	assertEquals(2,operateurService.retrieveAllOperateurs().size());
-//   	System.out.println("Retrieve all operators works !");
-//    }
-//	
-//	   @Test 
-//	   @Order(3)
-//	    public void UpdateOperateurTest() {
-//	    	when(operateurRepository.save(operateur1)).thenReturn(operateur1);
-//	    	assertNotNull(operateur1);
-//	    	assertEquals(operateur1, operateurService.updateOperateur(operateur1));
-//	    	System.out.println("Update operators works!");
-//	    }
-//	    
-//	    @Test
-//	    @Order(4)
-//	    public void retrieveOperateurTest() {
-//	    	when(operateurRepository.findById(operateur1.getIdOperateur())).thenReturn(Optional.of(operateur1));
-//	    	assertEquals(operateur1, operateurService.retrieveOperateur(operateur1.getIdOperateur()));
-//	    	System.out.println("Retrieve operator by id works !");
-//	    }
-//	
+ @Test 
+ @Order(2)
+	   public void retrieveAllOperateursTest() {
+	    	when(operateurRepository.findAll()).thenReturn(Stream
+	    			.of(operateur1,operateur2)
+	    			.collect(Collectors.toList()));
+	    	
+	    	assertEquals(2,operateurService.retrieveAllOperateurs().size());
+   	System.out.println("Retrieve all operators works !");
+    }
+	
+	   @Test 
+	   @Order(3)
+	    public void UpdateOperateurTest() {
+	    	when(operateurRepository.save(operateur1)).thenReturn(operateur1);
+	    	assertNotNull(operateur1);
+	    	assertEquals(operateur1, operateurService.updateOperateur(operateur1));
+	    	System.out.println("Update operators works!");
+	    }
+	    
+	    @Test
+	    @Order(4)
+	    public void retrieveOperateurTest() {
+	    	when(operateurRepository.findById(operateur1.getIdOperateur())).thenReturn(Optional.of(operateur1));
+	    	assertEquals(operateur1, operateurService.retrieveOperateur(operateur1.getIdOperateur()));
+	    	System.out.println("Retrieve operator by id works !");
+	    }
+	
 
 
 
