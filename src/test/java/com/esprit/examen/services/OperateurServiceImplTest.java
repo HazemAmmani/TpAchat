275bea03e3ad2510bf75f167703f3230f10c5ee0
hazemAm28 <ammani.hazem@esprit.tn>
2022-11-09 12:21:45 +0100

    changes pomxml

--- src/test/java/com/esprit/examen/services/OperateurServiceImplTest.java
+++ src/test/java/com/esprit/examen/services/OperateurServiceImplTest.java
@@ -5,10 +5,7 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-
-
-import org.junit.Test;
-
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 
 import org.springframework.beans.factory.annotation.Autowired;
